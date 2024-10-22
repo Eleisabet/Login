9687fbe64a0f7dcc9cf0439fd711aa9d9a17441b
Eleisabet <6175056@gmail.com>
2024-10-21 20:40:49 -0700

    xcode project added

---
+++ Login2/Login/SecondViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.swift
+//  Login
+//
+//  Created by Elizaveta Potapova on 10/20/24.
+//
+
+import Foundation
+import UIKit
+
+final class SecondViewController: UIViewController {
+    
+}
